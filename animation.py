1e824d5bd2f16c2535a3a869b98a0c2e531beba9
Edison Salazar <edisonsalazar@Edisons-MacBook-Pro.local>
2024-06-27 12:51:34 +0200

    Updating animation script

--- animation.py
+++ animation.py
@@ -4,30 +4,35 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from pysurf.database import PySurfDB
 from Bio.PDB.vectors import (Vector, calc_dihedral, calc_angle)
-from pysurf.system import Molecule
 from moviepy.editor import VideoFileClip, clips_array
 
 class PlotDB:
 
-    def __init__(self, output):
+    def __init__(self, output, traj_movie, plot_e):
+        self.plot_e = plot_e 
+        self.traj_movie = traj_movie 
         self.output = PySurfDB.load_database(output, read_only=True)
         self.nstates = self.output.dimensions["nstates"]
         self.fs = 0.02418884254
         self.ev = 27.211324570273
         self.aa = 0.529177208
         self.ene_min = np.array(self.output["energy"]).min()
+        self.ene_max = np.array(self.output["energy"]).max()
         self.times = self.output["time"][0:]*self.fs
         self.dim = len(self.times)
-        self.fs_rcParams = '18'
-        self.S0_energies =  (np.array(self.output["energy"])[:,0] - self.ene_min)*self.ev
-        self.S1_energies =  (np.array(self.output["energy"])[:,1] - self.ene_min)*self.ev
-
-        plt.rcParams['font.size'] = self.fs_rcParams
-        self.fig = plt.figure(figsize=(7, 10))
-        gs = self.fig.add_gridspec(2, 1, hspace=0)
-        self.ax0 = self.fig.add_subplot(gs[0])
-        self.ax1 = self.fig.add_subplot(gs[1])
-        self.transition_point, = self.ax0.plot([], [], 'o', color='black', markersize=10, zorder=5)
+        if self.plot_e == "ene":
+            plt.rcParams['font.size'] = '7' 
+            self.fig, self.ax = plt.subplots(figsize=(3.5, 2.57))
+            self.transition_point, = self.ax.plot([], [], 'o', color='black', markersize=5, zorder=5)
+        elif self.plot_e == "ene_ang":
+            plt.rcParams['font.size'] = '18' 
+            self.fig = plt.figure(figsize=(7, 10))
+            gs = self.fig.add_gridspec(2, 1, hspace=0)
+            self.ax0 = self.fig.add_subplot(gs[0])
+            self.ax1 = self.fig.add_subplot(gs[1])
+            self.transition_point, = self.ax0.plot([], [], 'o', color='black', markersize=10, zorder=5)
+        else:
+            raise SystemExit("This option does not exit")
 
     def dihedral(self, a, b, c, d):
         crd = self.output["crd"]
@@ -79,6 +84,7 @@ class PlotDB:
         return y_nan
 
     def plot_energy_angles_vs_time(self):
+        # Positions of atoms defined only for the CH2NH molecule 
         atom_1=0
         atom_2=1
         atom_3=2
@@ -88,7 +94,7 @@ class PlotDB:
         # the first subplot
         self.ax0.set_ylabel("Energy (eV)", fontweight = 'bold', fontsize = 16)
         self.ax0.set_xlim(self.times.min(), self.times.max())
-        self.ax0.set_ylim(min(self.S0_energies) - 1, max(self.S1_energies) + 1)
+        self.ax0.set_ylim(- 1, (self.ene_max-self.ene_min)*self.ev + 1)
         self.ax0.set_facecolor('white')
         handles1 = []
         labels1 = []
@@ -135,17 +141,31 @@ class PlotDB:
         self.ax1.legend(handles2,labels2,loc='upper center', bbox_to_anchor=(0.5, 2.27), prop={'size': 16}, ncol=len(labels2))
         return self.transition_point,
 
-    def init_plot(self):
-        self.ax.set_ylabel("Energy (eV)", fontweight = 'bold', fontsize = 16)
-        self.ax.set_xlabel("Time (fs)", fontweight = 'bold', fontsize = 16)
+    def plot_energy_vs_time(self):
+        self.ax.set_ylabel("Energy (eV)", fontweight = 'bold', fontsize = 8)
+        self.ax.set_xlabel("Time (fs)", fontweight = 'bold', fontsize = 8)
         self.ax.set_xlim(self.times.min(), self.times.max())
-        self.ax.set_ylim(min(self.S0_energies) - 1, max(self.S1_energies) + 1)
-        line1, = self.ax.plot(self.times, self.S1_energies, color='#ff7f0e', zorder=1)
-        line0, = self.ax.plot(self.times, self.S0_energies, color='#1f77b4', zorder=1)
+        self.ax.set_ylim(- 1, (self.ene_max-self.ene_min)*self.ev + 1)
+        self.ax.set_facecolor('white')
+        handles = []
+        labels = []
+        for i in range(self.nstates):
+            ene = np.array(self.output["energy"])[:,i] 
+            line, = self.ax.plot(self.times,(ene-self.ene_min)*self.ev)
+            handles.append(line)
+            labels.append('$S_%i$'%i)
         self.transition_point.set_data([], [])
-        handles = [line0,line1]
-        labels = ['$S_0$','$S_1$']
-        self.ax.legend(handles,labels,loc='upper center', bbox_to_anchor=(0.5, 1.15), prop={'size': 14}, ncol=2)
+        for i in range(1, self.dim):
+            if self.output["currstate"][i] != self.output["currstate"][i-1]:
+                curr = self.output["currstate"][i-1]
+                new = self.output["currstate"][i]
+                x = self.times[i]
+                line0 = self.ax.axvline(x[0],linestyle='--', c = 'purple')
+                handles.append(line0)
+                labels.append(fr"Hop:($S_{int(curr[0])} \to S_{int(new[0])}$) at {int(x[0])} fs")
+        plt.tight_layout()
+        #plt.subplots_adjust(top=0.9)  # Adjust the top padding as needed
+        self.ax.legend(handles,labels,loc='upper center', bbox_to_anchor=(0.5, 1.2), prop={'size': 8}, ncol=len(labels))
         return self.transition_point,
 
     def update_plot(self, i):
@@ -154,27 +174,26 @@ class PlotDB:
         self.transition_point.set_data(self.times[i], ene_curr)
         return self.transition_point,
 
-    def create_animation_3(self):
-        ani = animation.FuncAnimation(self.fig, self.update_plot, init_func=self.plot_energy_angles_vs_time, frames=len(self.times), blit=True)
-        ani.save('energy_animation_1.mp4', writer='ffmpeg', fps=30)
-        plt.close()
-
-    def create_animation(self):
-        ani = animation.FuncAnimation(self.fig, self.update_plot, init_func=self.init_plot, frames=len(self.times), blit=True)
-        ani.save('energy_animation_0.mp4', writer='ffmpeg', fps=30)
-        plt.close()
-
-    def create_animation_2(self, trajectory_file):
-        #ani = animation.FuncAnimation(self.fig, self.update_plot, init_func=self.init_plot, frames=len(self.times), blit=True)
-        ani = animation.FuncAnimation(self.fig, self.update_plot, init_func=self.plot_energy_angles_vs_time, frames=len(self.times), blit=True)
-        energy_animation_file = 'energy_animation.mp4'
+    def create_ene_ang_animation(self):
+        if self.plot_e == "ene":
+            ani = animation.FuncAnimation(self.fig, self.update_plot, init_func=self.plot_energy_vs_time, frames=len(self.times), blit=True)
+            energy_animation_file = 'ene_animation.mp4'
+        elif self.plot_e == "ene_ang":
+            ani = animation.FuncAnimation(self.fig, self.update_plot, init_func=self.plot_energy_angles_vs_time, frames=len(self.times), blit=True)
+            energy_animation_file = 'ene_ang_animation.mp4'
+        else:   
+            raise SystemExit("This option does not exit")
         Writer = animation.writers['ffmpeg']
         writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800)
         ani.save(energy_animation_file, writer=writer)
         plt.close()
+        return energy_animation_file
+
+    def create_animation_combined(self):
+        energy_animation_file = self.create_ene_ang_animation()
 
         # Load the trajectory video
-        trajectory_clip = VideoFileClip(trajectory_file)
+        trajectory_clip = VideoFileClip(self.traj_movie)
 
         # Load the energy animation video
         energy_clip = VideoFileClip(energy_animation_file)
@@ -183,15 +202,23 @@ class PlotDB:
         final_clip = clips_array([[trajectory_clip, energy_clip]])
 
         # Save the final combined video
-        final_clip.write_videofile('combined_video.mp4', fps=trajectory_clip.fps)
-
-def movie(traj_movie):
-    plot_db = PlotDB('results.db')
-    #plot_db.create_animation()
-    #plot_db.create_animation_3()
-    plot_db.create_animation_2(traj_movie)
+        if self.plot_e == "ene":
+            combined_animation_file = 'ene_combined_video.mp4'
+        elif self.plot_e == "ene_ang":
+            combined_animation_file = 'ene_combined_video.mp4'
+        else:   
+            raise SystemExit("This option does not exit")
+        final_clip.write_videofile(combined_animation_file, fps=trajectory_clip.fps)
 
 if __name__ == '__main__':
-    traj_movie = sys.argv[1]
-    movie(traj_movie)
+    # Database. In PySurf it is called results.db
+    db = sys.argv[1] 
+    # Trajectory movie saved in mp4 format
+    traj_movie = sys.argv[2]
+    # Type of movie: energies or energies and angles
+    type_movie = sys.argv[3]
+    # Calling the class PlotDB
+    plot_db = PlotDB(db,traj_movie,type_movie)
+    # Function to create an animation 
+    plot_db.create_animation_combined()
 
