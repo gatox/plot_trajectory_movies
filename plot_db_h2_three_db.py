b897d50803b1f81e3be401715c513db39124e49c
gatox <salazar.xavier@gmail.com>
2025-09-30 18:11:36 +0200

    script to generate plot for h2

---
+++ plot_db_h2_three_db.py
@@ -0,0 +1,168 @@
+import sys
+# import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+# from matplotlib import gridspec
+# from jinja2 import Template #build templates
+import numpy as np
+# from numpy import copy 
+from pysurf.database import PySurfDB
+# from pysurf.system import Molecule
+# from Bio.PDB.vectors import (Vector, calc_dihedral, calc_angle)
+# import matplotlib.animation as animation
+# from moviepy.editor import VideoFileClip, CompositeVideoClip, clips_array
+# from collections import namedtuple
+
+class PlotsH2:
+    
+    def __init__(self):
+        self.fs = 0.02418884254
+        self.ev = 27.211324570273
+        self.aa = 0.529177208
+        self.fs_rcParams = '20'
+        self.colors = plt.rcParams['axes.prop_cycle'].by_key()['color'][:3]
+        self.markers = list(Line2D.filled_markers)[:6]
+        self.titles = ["Noisless","Noise/Conv_Tol: 1.0e-2","Noise/Conv_Tol: 1.0e-3"]
+        
+    def read_db(self, output):
+        db = PySurfDB.load_database(output, read_only=True)
+        time = db["time"][0:]*self.fs
+        return db, time
+    
+    def dis_two_atmos(self, data, atom_1, atom_2):
+        atom_1 = int(atom_1)
+        atom_2 = int(atom_2)
+        dimer = []
+        for i,m in enumerate(data):
+            dimer.append(float(m[atom_2][2])-float(m[atom_1][2]))
+        return dimer
+    
+    def plot_pos_vel_3db(self,output_1,output_2,output_3):
+        db_1, _ = self.read_db(output_1)
+        db_2, _ = self.read_db(output_2)
+        db_3, _ = self.read_db(output_3)
+        fig, ax = plt.subplots()
+        crd_1 = np.array(self.dis_two_atmos(db_1["crd"], 0, 1))
+        vel_1 = np.array(self.dis_two_atmos(db_1["veloc"], 0, 1))
+        crd_2 = np.array(self.dis_two_atmos(db_2["crd"], 0, 1))
+        vel_2 = np.array(self.dis_two_atmos(db_2["veloc"], 0, 1))
+        crd_3 = np.array(self.dis_two_atmos(db_3["crd"], 0, 1))
+        vel_3 = np.array(self.dis_two_atmos(db_3["veloc"], 0, 1))
+        ax.plot(crd_1, vel_1 / 1e-3, color=self.colors[0], label = self.titles[0])
+        ax.plot(crd_2, vel_2 / 1e-3, color=self.colors[1], linestyle='--', label = self.titles[1])
+        ax.plot(crd_2, vel_3 / 1e-3, color=self.colors[2], linestyle='--', label = self.titles[2])
+        ax.scatter(crd_1[0], vel_1[0] / 1e-3, color='r', marker=self.markers[0], s=40, label='')
+        ax.scatter(crd_1[-1], vel_1[-1] / 1e-3, color='g', marker=self.markers[1], s=40, label='')
+        ax.scatter(crd_2[0], vel_2[0] / 1e-3, color='r', marker=self.markers[2], s=40, label='')
+        ax.scatter(crd_2[-1], vel_2[-1] / 1e-3, color='g', marker=self.markers[3], s=40, label='')
+        ax.scatter(crd_3[0], vel_3[0] / 1e-3, color='r', marker=self.markers[4], s=40, label='')
+        ax.scatter(crd_3[-1], vel_3[-1] / 1e-3, color='g', marker=self.markers[5], s=40, label='')
+
+        ax.set_xlabel('Position (a.u.)', fontweight='bold', fontsize=16)
+        ax.set_ylabel('Velocity (a.u.)', fontweight='bold', fontsize=16)
+
+        # Add scaling factor note for velocity axis
+        ax.text(
+            0.0, 1.0, "1e-3",
+            transform=ax.transAxes,  # relative to axes coords
+            ha='left', va='bottom',
+            fontsize=12
+        )
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), prop={'size': 12}, ncol=3,frameon=False)
+        fig.savefig("post_vel_h2_3.pdf", bbox_inches='tight')
+        plt.close(fig)
+    
+    def plot_pos_vel(self, output):
+        db, _ = self.read_db(output)
+        fig, ax = plt.subplots()
+        crd = np.array(self.dis_two_atmos(db["crd"], 0, 1))
+        vel = np.array(self.dis_two_atmos(db["veloc"], 0, 1))
+        # # Convert velocity to momentum (multiply by H mass in a.u.)
+        # m_H = 1837.15258739092  # hydrogen atom mass in a.u.
+        # mom = vel * m_H
+        ax.plot(crd, vel / 1e-3, color='blue')
+        ax.scatter(crd[0], vel[0] / 1e-3, color='r', s=40, label='Initial point')
+        ax.scatter(crd[-1], vel[-1] / 1e-3, color='g', s=40, label='Final point')
+
+        ax.set_xlabel('Position (a.u.)', fontweight='bold', fontsize=16)
+        ax.set_ylabel('Velocity (a.u.)', fontweight='bold', fontsize=16)
+
+        # Add scaling factor note for velocity axis
+        ax.text(
+            0.0, 1.0, "1e-3",
+            transform=ax.transAxes,  # relative to axes coords
+            ha='left', va='bottom',
+            fontsize=12
+        )
+        ax.legend()
+        fig.savefig("post_vel_h2.pdf", bbox_inches='tight')
+        plt.close(fig)
+        
+    def plot_time_total_energy_3db(self, output_1,output_2,output_3):
+        db_1, time_1 = self.read_db(output_1)
+        db_2, time_2 = self.read_db(output_2)
+        db_3, time_3 = self.read_db(output_3)
+        etot_1 = np.array(list(db_1["etot"]), dtype=float)
+        etot_2 = np.array(list(db_2["etot"]), dtype=float)
+        etot_3 = np.array(list(db_3["etot"]), dtype=float)
+        plt.plot(time_1,(etot_1-etot_1[0])/1e-3,color=self.colors[0], label = self.titles[0])
+        plt.plot(time_2,(etot_2-etot_2[0])/1e-3,color=self.colors[1], linestyle='--', label = self.titles[1]) 
+        plt.plot(time_3,(etot_3-etot_3[0])/1e-3,color=self.colors[2], linestyle='--', label = self.titles[2]) 
+        plt.axhline(0,linestyle=':', c = 'red')
+        plt.xlabel('Time (fs)', fontweight = 'bold', fontsize = 16)
+        plt.ylabel(r'$\Delta$ Total Energy (mHa)', fontweight = 'bold', fontsize = 16)
+        plt.xlim([0, 10])
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), prop={'size': 12}, ncol=3,frameon=False)
+        plt.savefig("time_etotal_h2_3.pdf", bbox_inches='tight')
+        plt.close()
+        
+    def plot_time_total_energy(self, output):
+        db, time = self.read_db(output) 
+        etot = np.array(list(db["etot"]), dtype=float)
+        plt.plot(time,(etot-etot[0])/1e-3,color='blue',label = '') 
+        plt.axhline(0,linestyle=':', c = 'red')
+        plt.xlabel('Time (fs)', fontweight = 'bold', fontsize = 16)
+        plt.ylabel(r'$\Delta$ Total Energy (mHa)', fontweight = 'bold', fontsize = 16)
+        plt.xlim([0, 10])
+        plt.savefig("time_etotal_h2.pdf", bbox_inches='tight')
+        plt.close()
+        
+    def plot_time_parmameter_3db(self, output_1,output_2,output_3):
+        db_1, time_1 = self.read_db(output_1)
+        db_2, time_2 = self.read_db(output_2)
+        db_3, time_3 = self.read_db(output_3)
+        parameter_1 = np.array(list(db_1["parameter"]), dtype=float)
+        parameter_2 = np.array(list(db_2["parameter"]), dtype=float)
+        parameter_3 = np.array(list(db_3["parameter"]), dtype=float)
+        plt.plot(time_1,parameter_1,color=self.colors[0], label = self.titles[0])
+        plt.plot(time_2,parameter_2,color=self.colors[1], linestyle='--', label = self.titles[1]) 
+        plt.plot(time_3,parameter_3,color=self.colors[2], linestyle='--', label = self.titles[2]) 
+        plt.xlabel('Time (fs)', fontweight = 'bold', fontsize = 16)
+        plt.ylabel(r'Optimal $\boldsymbol{\theta}$', fontweight = 'bold', fontsize=16)
+        plt.xlim([0, 10])
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), prop={'size': 12}, ncol=3,frameon=False)
+        plt.savefig("time_parameter_h2_3.pdf", bbox_inches='tight')
+        plt.close()
+        
+    def plot_time_parmameter(self, output):
+        db, time = self.read_db(output)
+        parameter = np.array(list(db["parameter"]), dtype=float)
+        plt.plot(time,parameter,color='blue',label = '') 
+        plt.xlabel('Time (fs)', fontweight = 'bold', fontsize = 16)
+        plt.ylabel(r'Optimal $\boldsymbol{\theta}$', fontweight = 'bold', fontsize=16)
+        plt.xlim([0, 10])
+        plt.savefig("time_parameter_h2.pdf", bbox_inches='tight')
+        plt.close()
+
+if __name__=="__main__":
+    db_1 = sys.argv[1]  #results.db  
+    db_2 = sys.argv[2]
+    db_3 = sys.argv[3]
+    picture = PlotsH2()
+    # picture.plot_pos_vel(db_1)
+    # picture.plot_time_total_energy(db_1)
+    # picture.plot_time_parmameter(db_1)
+    #picture.plot_pos_vel_3db(db_1,db_2,db_3)
+    # picture.plot_time_total_energy_3db(db_1,db_2,db_3)
+    picture.plot_time_parmameter_3db(db_1,db_2,db_3)
+    
