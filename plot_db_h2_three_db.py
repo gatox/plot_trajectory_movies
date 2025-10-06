2e776d8818b73a1416677b304493241728682c36
gatox <salazar.xavier@gmail.com>
2025-10-06 13:41:21 +0200

    update functions to evaluate 4 results.db

--- plot_db_h2_three_db.py
+++ plot_db_h2_three_db.py
@@ -20,9 +20,9 @@ class PlotsH2:
         self.ev = 27.211324570273
         self.aa = 0.529177208
         self.fs_rcParams = '20'
-        self.colors = plt.rcParams['axes.prop_cycle'].by_key()['color'][:3]
-        self.markers = list(Line2D.filled_markers)[:6]
-        self.titles = ["Noisless","Noise/Conv_Tol: 1.0e-2","Noise/Conv_Tol: 1.0e-3"]
+        self.colors = plt.rcParams['axes.prop_cycle'].by_key()['color'][:4]
+        self.markers = list(Line2D.filled_markers)[:8]
+        self.titles = ["Noisless","Noise/Conv_Tol: 1.0e-2","Noise/Conv_Tol: 1.0e-3", "Noise/Conv_Tol: 1.0e-4"]
         
     def read_db(self, output):
         db = PySurfDB.load_database(output, read_only=True)
@@ -37,10 +37,11 @@ class PlotsH2:
             dimer.append(float(m[atom_2][2])-float(m[atom_1][2]))
         return dimer
     
-    def plot_pos_vel_3db(self,output_1,output_2,output_3):
+    def plot_pos_vel_3db(self,output_1,output_2,output_3,output_4):
         db_1, _ = self.read_db(output_1)
         db_2, _ = self.read_db(output_2)
         db_3, _ = self.read_db(output_3)
+        db_4, _ = self.read_db(output_4)
         fig, ax = plt.subplots()
         crd_1 = np.array(self.dis_two_atmos(db_1["crd"], 0, 1))
         vel_1 = np.array(self.dis_two_atmos(db_1["veloc"], 0, 1))
@@ -48,15 +49,20 @@ class PlotsH2:
         vel_2 = np.array(self.dis_two_atmos(db_2["veloc"], 0, 1))
         crd_3 = np.array(self.dis_two_atmos(db_3["crd"], 0, 1))
         vel_3 = np.array(self.dis_two_atmos(db_3["veloc"], 0, 1))
+        crd_4 = np.array(self.dis_two_atmos(db_4["crd"], 0, 1))
+        vel_4 = np.array(self.dis_two_atmos(db_4["veloc"], 0, 1))
         ax.plot(crd_1, vel_1 / 1e-3, color=self.colors[0], label = self.titles[0])
         ax.plot(crd_2, vel_2 / 1e-3, color=self.colors[1], linestyle='--', label = self.titles[1])
-        ax.plot(crd_2, vel_3 / 1e-3, color=self.colors[2], linestyle='--', label = self.titles[2])
+        ax.plot(crd_3, vel_3 / 1e-3, color=self.colors[2], linestyle='--', label = self.titles[2])
+        ax.plot(crd_4, vel_4 / 1e-3, color=self.colors[3], linestyle='--', label = self.titles[3])
         ax.scatter(crd_1[0], vel_1[0] / 1e-3, color='r', marker=self.markers[0], s=40, label='')
         ax.scatter(crd_1[-1], vel_1[-1] / 1e-3, color='g', marker=self.markers[1], s=40, label='')
         ax.scatter(crd_2[0], vel_2[0] / 1e-3, color='r', marker=self.markers[2], s=40, label='')
         ax.scatter(crd_2[-1], vel_2[-1] / 1e-3, color='g', marker=self.markers[3], s=40, label='')
         ax.scatter(crd_3[0], vel_3[0] / 1e-3, color='r', marker=self.markers[4], s=40, label='')
         ax.scatter(crd_3[-1], vel_3[-1] / 1e-3, color='g', marker=self.markers[5], s=40, label='')
+        ax.scatter(crd_4[0], vel_4[0] / 1e-3, color='r', marker=self.markers[6], s=40, label='')
+        ax.scatter(crd_4[-1], vel_4[-1] / 1e-3, color='g', marker=self.markers[7], s=40, label='')
 
         ax.set_xlabel('Position (a.u.)', fontweight='bold', fontsize=16)
         ax.set_ylabel('Velocity (a.u.)', fontweight='bold', fontsize=16)
@@ -68,7 +74,8 @@ class PlotsH2:
             ha='left', va='bottom',
             fontsize=12
         )
-        ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), prop={'size': 12}, ncol=3,frameon=False)
+        plt.title("H2_dynamics/STO-3G/PNOF4/1000_shots/AER/IBM_pittsburgh/Opt_lvel=3", y=1.2)
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.195), prop={'size': 12}, ncol=2,frameon=False)
         fig.savefig("post_vel_h2_3.pdf", bbox_inches='tight')
         plt.close(fig)
     
@@ -98,21 +105,25 @@ class PlotsH2:
         fig.savefig("post_vel_h2.pdf", bbox_inches='tight')
         plt.close(fig)
         
-    def plot_time_total_energy_3db(self, output_1,output_2,output_3):
+    def plot_time_total_energy_3db(self, output_1,output_2,output_3,output_4):
         db_1, time_1 = self.read_db(output_1)
         db_2, time_2 = self.read_db(output_2)
         db_3, time_3 = self.read_db(output_3)
+        db_4, time_4 = self.read_db(output_4)
         etot_1 = np.array(list(db_1["etot"]), dtype=float)
         etot_2 = np.array(list(db_2["etot"]), dtype=float)
         etot_3 = np.array(list(db_3["etot"]), dtype=float)
+        etot_4 = np.array(list(db_4["etot"]), dtype=float)
         plt.plot(time_1,(etot_1-etot_1[0])/1e-3,color=self.colors[0], label = self.titles[0])
         plt.plot(time_2,(etot_2-etot_2[0])/1e-3,color=self.colors[1], linestyle='--', label = self.titles[1]) 
         plt.plot(time_3,(etot_3-etot_3[0])/1e-3,color=self.colors[2], linestyle='--', label = self.titles[2]) 
-        plt.axhline(0,linestyle=':', c = 'red')
+        plt.plot(time_4,(etot_4-etot_4[0])/1e-3,color=self.colors[3], linestyle='--', label = self.titles[3])
+        plt.axhline(0,linestyle=':', c = 'black')
         plt.xlabel('Time (fs)', fontweight = 'bold', fontsize = 16)
         plt.ylabel(r'$\Delta$ Total Energy (mHa)', fontweight = 'bold', fontsize = 16)
         plt.xlim([0, 10])
-        plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), prop={'size': 12}, ncol=3,frameon=False)
+        plt.title("H2_dynamics/STO-3G/PNOF4/1000_shots/AER/IBM_pittsburgh/Opt_lvel=3", y=1.2)
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.19), prop={'size': 12}, ncol=2,frameon=False)
         plt.savefig("time_etotal_h2_3.pdf", bbox_inches='tight')
         plt.close()
         
@@ -120,27 +131,31 @@ class PlotsH2:
         db, time = self.read_db(output) 
         etot = np.array(list(db["etot"]), dtype=float)
         plt.plot(time,(etot-etot[0])/1e-3,color='blue',label = '') 
-        plt.axhline(0,linestyle=':', c = 'red')
+        plt.axhline(0,linestyle=':', c = 'black')
         plt.xlabel('Time (fs)', fontweight = 'bold', fontsize = 16)
         plt.ylabel(r'$\Delta$ Total Energy (mHa)', fontweight = 'bold', fontsize = 16)
         plt.xlim([0, 10])
         plt.savefig("time_etotal_h2.pdf", bbox_inches='tight')
         plt.close()
         
-    def plot_time_parmameter_3db(self, output_1,output_2,output_3):
+    def plot_time_parmameter_3db(self, output_1,output_2,output_3,output_4):
         db_1, time_1 = self.read_db(output_1)
         db_2, time_2 = self.read_db(output_2)
         db_3, time_3 = self.read_db(output_3)
+        db_4, time_4 = self.read_db(output_4)
         parameter_1 = np.array(list(db_1["parameter"]), dtype=float)
         parameter_2 = np.array(list(db_2["parameter"]), dtype=float)
         parameter_3 = np.array(list(db_3["parameter"]), dtype=float)
+        parameter_4 = np.array(list(db_4["parameter"]), dtype=float)
         plt.plot(time_1,parameter_1,color=self.colors[0], label = self.titles[0])
         plt.plot(time_2,parameter_2,color=self.colors[1], linestyle='--', label = self.titles[1]) 
-        plt.plot(time_3,parameter_3,color=self.colors[2], linestyle='--', label = self.titles[2]) 
+        plt.plot(time_3,parameter_3,color=self.colors[2], linestyle='--', label = self.titles[2])
+        plt.plot(time_4,parameter_4,color=self.colors[3], linestyle='--', label = self.titles[3]) 
         plt.xlabel('Time (fs)', fontweight = 'bold', fontsize = 16)
         plt.ylabel(r'Optimal $\boldsymbol{\theta}$', fontweight = 'bold', fontsize=16)
         plt.xlim([0, 10])
-        plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), prop={'size': 12}, ncol=3,frameon=False)
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.19), prop={'size': 12}, ncol=2,frameon=False)
+        plt.title("H2_dynamics/STO-3G/PNOF4/1000_shots/AER/IBM_pittsburgh/Opt_lvel=3", y=1.2)
         plt.savefig("time_parameter_h2_3.pdf", bbox_inches='tight')
         plt.close()
         
@@ -158,11 +173,12 @@ if __name__=="__main__":
     db_1 = sys.argv[1]  #results.db  
     db_2 = sys.argv[2]
     db_3 = sys.argv[3]
+    db_4 = sys.argv[4]
     picture = PlotsH2()
-    # picture.plot_pos_vel(db_1)
-    # picture.plot_time_total_energy(db_1)
-    # picture.plot_time_parmameter(db_1)
-    #picture.plot_pos_vel_3db(db_1,db_2,db_3)
-    # picture.plot_time_total_energy_3db(db_1,db_2,db_3)
-    picture.plot_time_parmameter_3db(db_1,db_2,db_3)
+    #picture.plot_pos_vel(db_1)
+    #picture.plot_time_total_energy(db_1)
+    #picture.plot_time_parmameter(db_1)
+    picture.plot_pos_vel_3db(db_1,db_2,db_3,db_4)
+    picture.plot_time_total_energy_3db(db_1,db_2,db_3,db_4)
+    picture.plot_time_parmameter_3db(db_1,db_2,db_3,db_4)
     
